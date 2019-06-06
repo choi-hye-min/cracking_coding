29781dce085d22336cc35be50790a1197d9bb3fc
arthur <gogoke1@naver.com>
2019-06-06 23:40:48 +0900

    problem update

---
+++ src/problem/capter05_bit/Question5_1.java
@@ -0,0 +1,27 @@
+package problem.capter05_bit;
+
+import timecheck.ProcessTimeCheck;
+
+/**
+ * PAGE: 171
+ * 주제: 삽입
+ * 문제: 두개의 32비트 수 N가 M이 주어지고, 비트 위치 i와 j가 주어졌을때, M을 N에 삽입하는 메서드를 구현하라
+ *
+ * input: N = 10000000000, M = 10011, i = 2, j = 6
+ * output:N = 10001001100
+ */
+public class Question5_1 {
+    private static void solution(String str){
+        
+    }
+
+    private static void solutionBook(String str){
+        
+    }
+
+    public static void main(String[] args) {
+        ProcessTimeCheck processTimeCheck = new ProcessTimeCheck();
+        
+        processTimeCheck.processReport();
+    }
+}
